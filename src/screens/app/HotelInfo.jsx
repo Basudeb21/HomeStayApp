ae57c687e64fabfb0b90db8d039a9bfd0433a41e
Basudeb Paul Chowdhury <bpaulc21@gmail.com>
2025-04-15 11:38:53 +0530

    Project Setup

---
+++ src/screens/app/HotelInfo.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HotelInfo = () => {
+    return (
+        <View>
+            <Text>HotelInfo</Text>
+        </View>
+    )
+}
+
+export default HotelInfo
---
+++ src/screens/index.js
@@ -0,0 +1,15 @@
+// Auth Screens
+import Login from "./auth/Login";
+import Otp from "./auth/Otp";
+
+
+// App Screens
+import Home from "./app/Home";
+import HotelInfo from "./app/HotelInfo";
+import OfferedCities from "./app/offers/OfferedCities";
+import Offers from "./app/offers/Offers";
+import Profile from "./app/Profile";
+import Search from "./app/Search";
+import SearchResult from "./app/SearchResult";
+import Trips from "./app/Trips";
+
